a3a77869698c5fcc1a69415e16b00220382cd8ae
jzakotnik <jure@zakotnik.de>
2025-05-30 08:49:51 +0200

    changed dashboard image

--- src/components/FeatureDetails.jsx
+++ src/components/FeatureDetails.jsx
@@ -20,7 +20,7 @@ function FeatureDetails() {
         <div className="relative overflow-hidden pt-16">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <img
-              src={`${import.meta.env.BASE_URL}screenshot2.png`}
+              src={`${import.meta.env.BASE_URL}truspaceschema.png`}
               alt="App screenshot"
               className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-white/10"
               width="2432"
