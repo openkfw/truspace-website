5c7f1dbabfc22203e8eeb6cc341fc713d4e74d0b
Peter Molnar <p.molnar@accenture.com>
2025-06-04 15:54:39 +0200

    refactored LogoCloud

--- src/components/LogoCloud.jsx
+++ src/components/LogoCloud.jsx
@@ -2,43 +2,28 @@ function LogoCloud() {
   return (
     <div className="mx-auto mt-8 max-w-7xl px-6 sm:mt-16 lg:px-8">
       <h2 className="text-center text-lg/8 font-semibold text-white">
-        The world&#39;s most innovative companies use our app
+        Web technologies used in our app
       </h2>
-      <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+      <div className="mx-auto mt-10 grid grid-cols-4 items-center gap-x-8 gap-y-10">
         <img
-          className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          src="https://tailwindcss.com/plus-assets/img/logos/158x48/transistor-logo-white.svg"
-          alt="Transistor"
-          width="158"
-          height="48"
+          className="col-span-2 h-20 w-full object-contain lg:col-span-1"
+          src={`${import.meta.env.BASE_URL}ipfs-logo.svg`}
+          alt="ipfs"
         />
         <img
-          className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-white.svg"
-          alt="Reform"
-          width="158"
-          height="48"
+          className="col-span-2 h-14 w-full object-contain lg:col-span-1"
+          src={`${import.meta.env.BASE_URL}openwebui.svg`}
+          alt="openwebui"
         />
         <img
-          className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-white.svg"
-          alt="Tuple"
-          width="158"
-          height="48"
+          className="col-span-2 h-10 w-full object-contain lg:col-span-1"
+          src={`${import.meta.env.BASE_URL}nextjs-logo.svg`}
+          alt="nextjs"
         />
         <img
-          className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-          src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-white.svg"
-          alt="SavvyCal"
-          width="158"
-          height="48"
-        />
-        <img
-          className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-          src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-white.svg"
-          alt="Statamic"
-          width="158"
-          height="48"
+          className="col-span-2 h-12 w-full object-contain lg:col-span-1"
+          src={`${import.meta.env.BASE_URL}tailwind-css-logo.svg`}
+          alt="tailwindcss"
         />
       </div>
     </div>
